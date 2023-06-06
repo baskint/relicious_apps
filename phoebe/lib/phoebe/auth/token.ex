f808a05f956136d1edca4771e3248e57374d9f8c
Baskin Tapkan <baskint@gmail.com>
2023-06-06 11:48:17 -0500

    token verification

---
+++ phoebe/lib/phoebe/auth/token.ex
@@ -0,0 +1,6 @@
+defmodule Phoebe.Auth.Token do
+  @moduledoc """
+  Module that will verify the JWT token passed in between apps
+  """
+  use Joken.Config
+end
--- phoebe/lib/phoebe_web/controllers/authentication_controller.ex
+++ phoebe/lib/phoebe_web/controllers/authentication_controller.ex
@@ -1,65 +1,40 @@
 defmodule PhoebeWeb.AuthenticationController do
   use Phoenix.Controller
+  import Joken
+  import Plug.Conn
 
   def authenticate(conn, _params) do
     jwt_secret = Application.fetch_env!(:phoebe, :jwt_secret)
-    IO.puts("jwt_secret")
-    IO.inspect(jwt_secret)
     token = Phoenix.Token.sign(conn, "jwt_secret", jwt_secret)
-    authenticated = check_authentication(conn)
-
-    json(conn, %{authenticated: authenticated})
+    authenticated = check_authentication(conn, _params)
   end
 
-  defp check_authentication(conn) do
-    # figure this out
-    true
+  defp check_authentication(conn, _params) do
+    token = get_token_from_request(conn)
+
+    jwt_secret = Application.fetch_env!(:phoebe, :jwt_secret)
+
+    signer = Joken.Signer.create("HS256", jwt_secret)
+
+    case Phoebe.Auth.Token.verify_and_validate(token, signer) do
+      {:ok, %{"email" => email, "name" => name}} ->
+        IO.puts "received: #{name} #{email}"
+        json(conn, %{authenticated: true})
+
+      {:error, {test}} ->
+        IO.puts(test)
+        json(conn, %{authenticated: false})
+    end
   end
 
   defp get_token_from_request(conn) do
-    case get_req_header(conn, "authorization") do
+    case Plug.Conn.get_req_header(conn, "authorization") do
       [header_value | _] ->
-        String.split(header_value, "Bearer ")[-1]
+        [_, token] = String.split(header_value, "Bearer ")
+        token
 
       _ ->
         nil
     end
   end
-
-  # defp check_authentication(conn) do
-  #   case validate_token(conn) do
-  #     {:ok, _claims} ->
-  #       # Token is valid, return 200 OK
-  #       send_resp(conn, 200, "Token is valid")
-
-  #     {:error, reason} ->
-  #       # Token is invalid, return 401 Unauthorized
-  #       send_resp(conn, 401, "Token is invalid")
-  #   end
-  # end
-
-  # defp validate_token(conn) do
-  #   # Retrieve the JWT token from the request headers or other source
-  #   token = get_token_from_request(conn)
-
-  #   case Guardian.decode_and_verify(token) do
-  #     {:ok, claims} ->
-  #       {:ok, claims}
-
-  #     {:error, _reason} ->
-  #       {:error, "Invalid token"}
-  #   end
-  # end
-
-  # defp get_token_from_request(conn) do
-  #   # Retrieve the JWT token from the request headers or other source
-  #   # Example: If the token is passed in the "Authorization" header as "Bearer <token>"
-  #   case get_req_header(conn, "authorization") do
-  #     [header_value | _] ->
-  #       String.split(header_value, "Bearer ")[-1]
-
-  #     _ ->
-  #       nil
-  #   end
-  # end
 end
